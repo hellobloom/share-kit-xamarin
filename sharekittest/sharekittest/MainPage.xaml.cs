583225153b1459db450925a3232e26cb0a6c4917
mulili <mulili.nzuki@gmail.com>
2019-06-26 01:05:48 +0300

    minor changes

--- sharekittest/sharekittest/MainPage.xaml.cs
+++ sharekittest/sharekittest/MainPage.xaml.cs
@@ -19,16 +19,17 @@ namespace sharekittest
 
 
             // set the request data
-            var requestData = new sharekit.RequestData();
-
-            requestData.action = sharekit.RequestData.Action_attestation;
-            requestData.token = "0x8f31e48a585fd12ba58e70e03292cac712cbae39bc7eb980ec189aa88e24d043";
-            requestData.url = "https://receive-kit.bloom.co/api/receive";
-            requestData.org_logo_url = "https://bloom.co/images/notif/bloom-logo.png";
-            requestData.org_name = "Bloom";
-            requestData.org_usage_policy_url = "https://bloom.co/legal/terms";
-            requestData.org_privacy_policy_url = "https://bloom.co/legal/privacy";
-            requestData.types = new string[] { "full-name", "phone", "email" };
+            var requestData = new sharekit.RequestData
+            {
+                action = sharekit.RequestData.Action_attestation,
+                token = "0x8f31e48a585fd12ba58e70e03292cac712cbae39bc7eb980ec189aa88e24d043",
+                url = "https://receive-kit.bloom.co/api/receive",
+                org_logo_url = "https://bloom.co/images/notif/bloom-logo.png",
+                org_name = "Bloom",
+                org_usage_policy_url = "https://bloom.co/legal/terms",
+                org_privacy_policy_url = "https://bloom.co/legal/privacy",
+                types = new string[] { "full-name", "phone", "email" }
+            };
 
             // set the callback url
             var callBackUrl = "https://google.com";
